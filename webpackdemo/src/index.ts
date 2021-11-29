f83f778e3575b327fdc9a56d10aa3f52ecad8e4b
jiaotong <739493247@qq.com>
2021-11-29 18:10:13 +0800

    兼容旧版js

--- webpackdemo/src/index.ts
+++ webpackdemo/src/index.ts
@@ -4,6 +4,10 @@ function sum(a: number, b: number): number {
     return a + b
 }
 
-console.log(sum(123,678))
+const obj = {name: "孙悟空", age: 33}
+console.log(obj);
+obj.age = 18;
+console.log(obj);
+console.log(sum(123, 678))
 console.log('哈哈');
 console.log(hi)
--- webpackdemo/webpack.config.js
+++ webpackdemo/webpack.config.js
@@ -20,7 +20,34 @@ module.exports = {
             {
                 //用ts-loader处理以ts结尾的文件
                 test: /\.ts$/,
-                use: 'ts-loader',
+                //先用ts-loader编译  再用babel将编译后的文件转为旧版js
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {//配置babel
+                            //设置预定义环境
+                            presets: [
+                                [
+                                    //指定环境插件
+                                    "@babel/preset-env",
+                                    //配置信息
+                                    {
+                                        //要兼容的目标浏览器
+                                        targets: {
+                                            "chrome": "58",
+                                            "ie": "11"
+                                        },
+                                        //指定corejs版本
+                                        "corejs": "3",
+                                        //使用corejs的方式 usage表示按需加载
+                                        "useBuiltIns": "usage"
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    'ts-loader'
+                ],
                 //要排除的文件
                 exclude: /node_modules/
             }
